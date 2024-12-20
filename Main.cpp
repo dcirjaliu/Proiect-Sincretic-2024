ff651b630648545b971edddd6357cc3878faafc1
Diana-Maria Cîrjaliu <diana.maria.cirjaliu@gmail.com>
2024-12-20 13:05:02 +0200

    Schimbare
    Am modificat fisierul Dockerfile deoarece aparent nu se updata automat si am mai modificat codul, ca sa fie putin mai usor

--- Main.cpp
+++ Main.cpp
@@ -1,42 +1,59 @@
 #include<iostream>
 
-int n=8;    //setez 8 regine
+const int n=8;    //setez 8 regine
+int v[n];   //vectorul care pastreaza coloana fiecarei regine
+int tabla_sah[n][n];    //matricea care reprezinta tabla de sah
 
 /*Problema celor 8 regine: să se scrie un program care plasează 8 regine pe tabla de
 șah, fără ca acestea să se atace reciproc*/
 
 using namespace std;
 
-//voi marca tabla cu 0 intial, iar daca o regina este asezata voi inlocui cu 1
-
-void InitializareMatrice(int n,int a[][8])  //intializarea matrice cu 0
+//verificam daca putem plasa regina pe linia k, coloana v[k]
+int PlasareCorecta(int k)
 {
-    for(int i=0;i<n;i++)
-    {
-        for(int j=0;j<n;j++)
-        {
-            a[i][j]=0;
-        }
-    }
-}
-
-int ReginaPlasataCorect(int n,int v[],int k)
-{
-    //v[]-vector care pastreaza coloanele in care sunt plasate reginele pe fiecare linie a tablei
-    //v[i]=3 regina de pe linia i este plasata in coloana 3
-
-    //verificam daca reginele se ataca pe aceeasi coloana sau diagonala
     for(int i=0;i<k;i++)
     {
-        if(v[i]==v[k]||v[i]==v[k]+(k-i)||v[i]==v[k]-(k-i))
+        //verificam daca reginile sunt pe aceeasi coloana sau pe aceeasi diagonala
+        if(v[i]==v[k]||v[i]-i==v[k]-k||v[i]+i==v[k]+k)
+            return 0;   //plasare incorecta, reginile se ataca
+    }
+
+    return 1;   //plasare corecta, reginele nu se ataca
+}
+
+//functia de plasare a reginelor
+void PlasareRegine(int k)
+{
+    if(k==n)    //daca am plasat toate reginele
+    {
+        for(int i=0;i<n;i++)
         {
-            
+            for(int j=0;j<n;j++)
+            {
+                if(v[i]==j)
+                    tabla_sah[i][j]=1;  //plasam regina
+
+                cout<<tabla_sah[i][j]<<" "; //afisam tabla
+            }
         }
+
+        return; //iesim din functie
+    }
+
+    //incercam sa plasam regina pe fiecare coloana a linei curente
+    for(int i=0;i<n;i++)
+    {
+        v[k]=i; //plasam regina pe linia k, coloana i
+
+        if(PlasareCorecta(k))   //daca plasare este sigura
+            PlasareRegine(k+1); //plasam regina pe linia urmatoare
     }
 }
 
 int main()
 {
-   
+    PlasareRegine(0);   //incepem cu linia 0
+
     return 0;
 }
