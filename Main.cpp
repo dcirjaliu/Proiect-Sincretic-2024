4fd9fa59a4be3f71bf31fdbe54b9a663971f8ad1
Diana-Maria Cîrjaliu <diana.maria.cirjaliu@gmail.com>
2024-12-20 12:20:12 +0200

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,10 +1,42 @@
 #include<iostream>
 
+int n=8;    //setez 8 regine
+
+/*Problema celor 8 regine: să se scrie un program care plasează 8 regine pe tabla de
+șah, fără ca acestea să se atace reciproc*/
+
 using namespace std;
+
+//voi marca tabla cu 0 intial, iar daca o regina este asezata voi inlocui cu 1
+
+void InitializareMatrice(int n,int a[][8])  //intializarea matrice cu 0
+{
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            a[i][j]=0;
+        }
+    }
+}
+
+int ReginaPlasataCorect(int n,int v[],int k)
+{
+    //v[]-vector care pastreaza coloanele in care sunt plasate reginele pe fiecare linie a tablei
+    //v[i]=3 regina de pe linia i este plasata in coloana 3
+
+    //verificam daca reginele se ataca pe aceeasi coloana sau diagonala
+    for(int i=0;i<k;i++)
+    {
+        if(v[i]==v[k]||v[i]==v[k]+(k-i)||v[i]==v[k]-(k-i))
+        {
+            
+        }
+    }
+}
 
 int main()
 {
-    cout<<"Aceasta este prima mea linie de cod\n";
-
+   
     return 0;
 }
